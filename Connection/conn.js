b4857a839b595faeb89a7cabb500875a786cc169
EshamAfzal <221008@students.au.edu.pk>
2024-12-30 00:04:44 +0500

    initial backend commit trying to add some basic mongodb functionalityi with some backend code

---
+++ Connection/conn.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yourDatabaseName', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("Connected to MongoDB successfully!");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB:", err.message);
+});
---
+++ Controllers/user.js
@@ -0,0 +1,48 @@
+const User = require('../Modals/user');
+const bcrypt = require('bcryptjs');
+
+
+
+exports.signUp = async(req,res)=>{
+    try{
+        const { channelName, userName, about, profilePic, password } = req.body;
+        const isExist = await User.findOne({ userName });
+        console.log(isExist);
+       if(isExist)
+       {
+        res.status(400).json({error:"username already exists please try with other username "})
+       }else
+       {
+           let updatedpass = await bcrypt.hash(password,10);
+           const user = new user({channelName, UserName,about, profilepic,password:updatedpass});
+           await user.save();
+           res.status(201).json({message:"user registered successfully",success:"yes",data:user});
+        }
+        
+        
+    } catch (error){
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+exports.signIn = async (req,res)=>{
+    try{
+        const { userName, password } = req.body;
+        const user = await User.findOne({ userName });
+        
+        if(user && await bcrypt.compare(password,user.password) ){
+
+            res.json({message:"Logged in successfully", success:"true"});
+        }
+        else
+        {
+            res.status(400).json({error:"Invalid Credentials"});
+        }
+    } catch (errorMsg){
+        res.status(500).json({ error: 'Server error' });
+    }
+}
+
+exports.logout = async(req,res)=>{
+    res.clearCookie('token', cookieOptions).json({ message: 'Logged out successfully' });
+}
