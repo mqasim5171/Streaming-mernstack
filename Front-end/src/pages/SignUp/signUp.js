69e75e3ad8043a0caf4954502ad8331e83b857c7
mqasim5171 <mqasim5171@gmail.com>
2025-01-04 21:48:46 +0500

    fixed issues with capital P

--- Front-end/src/pages/SignUp/signUp.js
+++ Front-end/src/pages/SignUp/signUp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './signUp.css';
+import './signUP.css';
 import YouTubeIcon from '@mui/icons-material/YouTube';
 import { Link,useNavigate } from 'react-router-dom';
 import axios from 'axios';
