347b8096270e3da27916ba4a183083dedcbd5576
mqasim5171 <mqasim5171@gmail.com>
2025-01-06 04:52:42 +0500

    played with backend  little bit

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 var express = require("express");
-const cookieParser = require('cookie-parser');
-
 var app = express();
 var port = 4000
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Your React app's URL
+    credentials: true
+  }))
 
 app.use(express.json());
 app.use(cookieParser());
 require('./Connection/conn');
 
-
-
 const AuthRoutes = require('./Routes/user');
 const VideoRoutes = require('./Routes/video');
 const CommentRoutes = require('./Routes/comment');
-
-
 
 app.use('/auth',AuthRoutes);
 app.use('/api',VideoRoutes);
